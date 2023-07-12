ca117aa08d998d66141636ff4f81e932bec1419b
Karsten <karstenwestra+github@gmail.com>
2023-07-12 21:47:14 +0200

    Feature/fixed framerate (#2)
    * Enforced framerate through SDL. Added _rdtsc cycle count for profiling purposes.
    
    ---------
    
    Co-authored-by: Karsten Westra <karstenwestra@gmail.com>

--- src/main.cc
+++ src/main.cc
@@ -2,10 +2,30 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 
+#include <x86intrin.h>
+
 #include "defs.h"
 #include "os_window.cc"
 #include "os_input.cc"
 #include "gamelib.cc"
+#include "framerate.cc"
+
+internal int GetWindowRefreshRate(SDL_Window *Window)
+{
+    SDL_DisplayMode Mode;
+    int DisplayIndex = SDL_GetWindowDisplayIndex(Window);
+
+    int DefaultRefreshRate = 60;
+    if (SDL_GetDesktopDisplayMode(DisplayIndex, &Mode) != 0)
+    {
+        return DefaultRefreshRate;
+    }
+    if (Mode.refresh_rate == 0)
+    {
+        return DefaultRefreshRate;
+    }
+    return Mode.refresh_rate;
+}
 
 int main(int Argc, char *Args[])
 {
@@ -36,6 +56,20 @@ int main(int Argc, char *Args[])
     game_state GameState = {};
     GameState.Running = true;
 
+    int const GameUpdateHz = 30;
+    real32 TargetSecondsPerFrame = 1.0f / (real64)GameUpdateHz;
+    int DetectedFrameRate = GetWindowRefreshRate(WindowState.Window);
+    if (DetectedFrameRate != GameUpdateHz)
+    {
+        printf("Device capable refresh rate is %d Hz, but Game runs in %d Hz\n", DetectedFrameRate, GameUpdateHz);
+    }
+    // Ticks per second for this CPU.
+    u64 PerfCountFrequency = SDL_GetPerformanceFrequency();
+
+    // Administration to enforce a framerate of GameUpdateHz
+    u64 StartCycleCount = _rdtsc();
+    u64 StartTime = SDL_GetPerformanceCounter();
+
     while (GameState.Running)
     {
         ProcessInput(&WindowState, &GameState, &BackBuffer);
@@ -49,7 +83,34 @@ int main(int Argc, char *Args[])
         // TODO: simulate game
         GameLib.GameUpdateAndRender(&GameState, &BackBuffer);
 
+        TryWaitForNextFrame(StartTime, TargetSecondsPerFrame);
+
+        // Measure the Game time, ignore SDL Update time.
+        u64 EndTime = SDL_GetPerformanceCounter();
+
         UpdateWindow(&WindowState, BackBuffer.Pixels);
+
+        /// 
+        /// Profiling
+        ///
+
+        // Cycles
+        u64 EndCycleCount = _rdtsc();
+        u64 CycleCount = EndCycleCount - StartCycleCount;
+        // Accumulate
+        u64 ElapsedTime = EndTime - StartTime;
+
+        /// Visualise
+        // Time: Convert frametime in (ms/f)
+        real64 MSPF = (1000.0f * (real64)ElapsedTime) / (real64)PerfCountFrequency;
+        // FPS: compare CPU counter with ticks per seconds to Rate (f/s)
+        real64 FPS = (real64)PerfCountFrequency / (real64)ElapsedTime;
+
+        // Cycles devided by a million = Mega Cycles per frame (mc/f).
+        real64 MCPF = ((real64)CycleCount / (1000.0f * 1000.0f));
+        printf("Time: %.02fms/f, FPS: %.02ff/s, Cycles: %.02fmc/f\n", MSPF, FPS, MCPF);
+        StartCycleCount = EndCycleCount;
+        StartTime = EndTime;
     }
 
     DestroyWindow(&WindowState);
